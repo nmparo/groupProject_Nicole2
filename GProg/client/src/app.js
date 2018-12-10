9cab4336f5cc4aba4be4fd2058f9b3d166a29c2b
Nicole <nmparo@uwm.edu>
2018-12-10 10:29:05 -0600

    Initial Commit

---
+++ GProg/client/src/app.js
@@ -0,0 +1,31 @@
+import {AuthorizeStep} from 'aurelia-auth';
+
+export class App {
+  configureRouter(config, router) {
+    this.router = router;
+    config.addPipelineStep('authorize', AuthorizeStep); 
+
+    config.map([
+      {
+        route: ['', 'landing'],
+        moduleId: './modules/landing',
+        name: 'Landing',
+        auth: false
+
+      },
+      {
+        route: 'home',
+        moduleId: './modules/home',
+        name: 'Home',
+        auth: true
+
+      },
+      {
+        route: 'users',
+        moduleId: './modules/users',
+        name: 'Users',
+        auth: true
+      }
+    ]);
+  }
+}
